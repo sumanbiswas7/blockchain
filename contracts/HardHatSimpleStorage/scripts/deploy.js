02b8d0cf832016a2a6a1f040604d3aa80d2d7529
suman <ronaldosuman97@gmail.com>
2022-07-19 16:21:05 +0530

    chore: [HardHatSimpleStorage] implementation done

---
+++ contracts/HardHatSimpleStorage/scripts/deploy.js
@@ -0,0 +1,20 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+// const hre = require("hardhat");
+const { ethers } = require("hardhat");
+
+async function main() {
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+  console.log("Deploying contract ...");
+  const simpleStorage = await SimpleStorageFactory.deploy();
+  console.log(`Deployed at - ${simpleStorage.address}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
